03691192140e2ca8d3839f1dbd6d184ca7a425c3
Willie Wheeler <willie.wheeler@gmail.com>
2011-11-03 00:22:33 -0700

    Removed @Transactional from AbstractHbnDao because recipe 10.3 assumes that we have direct access to the DAOs, rather than going through proxies.

--- api/src/main/java/com/springinpractice/dao/hibernate/AbstractHbnDao.java
+++ api/src/main/java/com/springinpractice/dao/hibernate/AbstractHbnDao.java
@@ -12,15 +12,17 @@ import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
-import javax.persistence.Transient;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ReflectionUtils;
 
 import com.springinpractice.dao.Dao;
+
+// Don't put @Transactional here. It's not that it's inherently wrong--indeed it would allow us to avoid some pass-
+// though service bean methods--but using @Transactional causes Spring to create proxies, and recipe 10.3 assumes that
+// it has direct access to the DAOs. I don't think we're doing direct DAO injects into controllers anywhere. [WLW]
 
 /**
  * @version $Id: AbstractHbnDao.java 16 2011-04-18 05:29:18Z willie $
@@ -45,7 +47,6 @@ public abstract class AbstractHbnDao<T extends Object> implements Dao<T> {
 	
 	private String getDomainClassName() { return getDomainClass().getName(); }
 	
-	@Transactional(readOnly = false)
 	public void create(T t) {
 		
 		// If there's a setDateCreated() method, then set the date.
@@ -63,40 +64,32 @@ public abstract class AbstractHbnDao<T extends Object> implements Dao<T> {
 	}
 	
 	@SuppressWarnings("unchecked")
-	@Transactional(readOnly = true)
 	public T get(Serializable id) {
 		return (T) getSession().get(getDomainClass(), id);
 	}
 	
 	@SuppressWarnings("unchecked")
-	@Transactional(readOnly = true)
 	public T load(Serializable id) {
 		return (T) getSession().load(getDomainClass(), id);
 	}
 	
 	@SuppressWarnings("unchecked")
-	@Transactional(readOnly = true)
 	public List<T> getAll() {
 		Query q = getSession().createQuery("from " + getDomainClassName());
 		return q.list();
 	}
 	
-	@Transactional(readOnly = false)
 	public void update(T t) { getSession().update(t); }
 	
-	@Transactional(readOnly = false)
 	public void delete(T t) { getSession().delete(t); }
 	
-	@Transactional(readOnly = false)
 	public void deleteById(Serializable id) { delete(load(id)); }
 	
-	@Transactional(readOnly = false)
 	public void deleteAll() {
 		Query q = getSession().createQuery("delete " + getDomainClassName());
 		q.executeUpdate();
 	}
 	
-	@Transactional(readOnly = true)
 	public long count() {
 		Query q = getSession()
 			.createQuery("select count(*) from " + getDomainClassName());
