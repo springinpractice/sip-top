9d267bb7b4d36ef196e87a65cbec50de57fc32f3
Willie Wheeler <willie.wheeler@gmail.com>
2011-11-11 11:04:45 -0700

    added spring milestone repo, added spring data mongodb dep

--- api/src/main/java/com/springinpractice/dao/Dao.java
+++ api/src/main/java/com/springinpractice/dao/Dao.java
@@ -1,15 +1,9 @@
-/* 
- * $Id: Dao.java 12 2010-06-28 08:20:56Z  $
- * 
- * Copyright (c) 2010 Manning Publication Co.
- */
 package com.springinpractice.dao;
 
 import java.io.Serializable;
 import java.util.List;
 
 /**
- * @version $Id: Dao.java 12 2010-06-28 08:20:56Z  $
  * @author Willie Wheeler (willie.wheeler@gmail.com)
  */
 public interface Dao<T extends Object> {
@@ -25,7 +19,7 @@ public interface Dao<T extends Object> {
 	void create(T t);
 	
 	/**
-	 * Finds the requested object in the data store and returns it, or null if there is no such persistent instance.
+	 * Finds the requested object in the repository and returns it, or null if there is no such persistent instance.
 	 * 
 	 * @param id ID
 	 * @return requested object, or null
@@ -35,8 +29,8 @@ public interface Dao<T extends Object> {
 	/**
 	 * <p>
 	 * Returns either a proxy for the requested object (one having the right class and ID), or else the actual object
-	 * if it's available without hitting the data store (e.g. in cache). The basic idea behind this method is to allow
-	 * apps establish references to the requested object without requiring a call to the data store.
+	 * if it's available without hitting the repository (e.g. in cache). The basic idea behind this method is to allow
+	 * apps establish references to the requested object without requiring a call to the repository.
 	 * </p>
 	 * <p>
 	 * Use this method only if you assume the instance actually exists; i.e., non-existence is an exception.
